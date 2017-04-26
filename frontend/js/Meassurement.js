3b6683efa316809c3b272eabecc944f97a275780
XieLong <XievenLonger@users.noreply.github.com>
2017-04-26 15:00:05 +0200

    Preparations for physics in the front end.

--- frontend/js/Meassurement.js
+++ frontend/js/Meassurement.js
@@ -32,25 +32,49 @@ class Measurement {
 		return measurement;
 	}
 
-	static fromPixel(pixel) {
+	static forPixel(pixel) {
 		let measurement = new Measurement();
 		measurement.setPixel(pixel);
 		return measurement;
 	}
 
-	static fromMetres(metres) {
+	static forMetres(metres) {
 		let measurement = new Measurement();
 		measurement.setMetres(metres);
 		return measurement;
 	}
 
+	static randomPixel(minPixel, maxPixel) {
+		return Measurement.forPixel(randomInt(minPixel, maxPixel));
+	}
+
+	static randomMetres(minMetres, maxMetres) {
+		return Measurement.forMetres(random(minMetres, maxMetres));
+	}
+
 	setPixel(pixel) {
 		this.pixel = pixel;
 		this.metres = pixel / Constants.PIXEL_PER_METRE;
+		// this.validateMetres();
 	}
 
 	setMetres(metres) {
 		this.metres = metres;
 		this.pixel = metres * Constants.PIXEL_PER_METRE;
+		// this.validateMetres();
 	}
+
+	validateMetres() {
+		if (this.metres < 0) {
+			throw "Negative measurement can't be interpreted by physics or graphics!";
+		}
+		if (this.metres < 0.1 || this.metres > 10) {
+			console.warn("Measurement should not be smaller than 0.1 metres (" +
+				(0.1 * Constants.PIXEL_PER_METRE) +
+				" pixels) or bigger than 10 metres (" +
+				(10 * Constants.PIXEL_PER_METRE) +
+				" pixels), otherwise the physics simulation gets unreliable.")
+		}
+	}
+
 }
