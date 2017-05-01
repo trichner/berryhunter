0332b8a90b4dcef19bb27718ade51ed1ba30f9f2
Thomas Richner <thomasrichner@oviva.ch>
2017-05-01 18:14:19 +0200

    Added playerid to gamestate.

--- backend/net.go
+++ backend/net.go
@@ -62,6 +62,7 @@ func (n *NetSystem) Update(dt float32) {
 		// copy gameStatePrototype
 		clientGameState := gameState
 		clientGameState.ClientID = player.client.id
+		clientGameState.PlayerID = player.ID()
 		msg := &MessageDTO{"GAME_STATE", clientGameState}
 		msgJson, _ := json.Marshal(msg)
 		player.client.Tx(&Message{string(msgJson)})
