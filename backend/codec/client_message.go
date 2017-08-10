09653904a56c27c8ccb142f9f38555e0c746c9cd
Thomas Richner <thomasrichner@oviva.ch>
2017-08-10 19:45:47 +0200

    Unwrap client input messages.

---
+++ backend/codec/client_message.go
@@ -0,0 +1,65 @@
+package codec
+
+import (
+	"github.com/trichner/berryhunter/api/schema/BerryhunterApi"
+	"github.com/google/flatbuffers/go"
+	"github.com/trichner/berryhunter/backend/model"
+	"github.com/trichner/berryhunter/backend/phy"
+	"github.com/trichner/berryhunter/backend/items"
+	"log"
+)
+
+func unwrapInput(msg *BerryhunterApi.ClientMessage) *BerryhunterApi.Input {
+
+	unionTable := new(flatbuffers.Table)
+	if msg.Body(unionTable) {
+		i := &BerryhunterApi.Input{}
+		i.Init(unionTable.Bytes, unionTable.Pos)
+		return i
+	}
+	return nil
+}
+
+func unmarshalInput(fbInput *BerryhunterApi.Input) *model.PlayerInput {
+	if fbInput == nil {
+		return nil
+	}
+
+	i := &model.PlayerInput{}
+
+	// umarshal simple scalars
+	i.Tick = fbInput.Tick()
+	i.Rotation = fbInput.Rotation()
+
+	// parse Movement if existing
+	m := fbInput.Movement(nil)
+	if m != nil {
+		i.Movement = &phy.Vec2f{
+			X: m.X(),
+			Y: m.Y(),
+		}
+	}
+
+	// parse Action if existent
+	a := fbInput.Action(nil)
+	if a != nil {
+		i.Action = &model.Action{
+			Item: items.ItemEnum(a.Item()),
+			Type: model.ActionType(a.ActionType()),
+		}
+	}
+	return i
+}
+
+func ClientMessageFlatbufferUnmarshal(bytes []byte) *model.PlayerInput {
+
+	msg := BerryhunterApi.GetRootAsClientMessage(bytes, 0)
+
+	if (msg.BodyType() == BerryhunterApi.ClientMessageBodyInput) {
+		return unmarshalInput(unwrapInput(msg))
+	}
+
+	log.Printf("Unhandled client message: %s (%d)", BerryhunterApi.EnumNamesClientMessageBody[int(msg.BodyType())], msg.BodyType())
+
+	return nil
+}
--- backend/input.go
+++ backend/input.go
@@ -10,6 +10,7 @@ import (
 	"github.com/trichner/berryhunter/backend/items"
 	"github.com/trichner/berryhunter/backend/model"
 	"github.com/trichner/berryhunter/backend/model/placeable"
+	"github.com/trichner/berryhunter/backend/codec"
 )
 
 
@@ -32,34 +33,6 @@ type PlayerInputSystem struct {
 	receive chan *clientMessage
 }
 
-func PlayerInputFlatbufferUnmarshal(bytes []byte) *model.PlayerInput {
-
-	i := &model.PlayerInput{}
-	fbInput := BerryhunterApi.GetRootAsInput(bytes, 0)
-
-	// umarshal simple scalars
-	i.Tick = fbInput.Tick()
-	i.Rotation = fbInput.Rotation()
-
-	// parse Movement if existing
-	m := fbInput.Movement(nil)
-	if m != nil {
-		i.Movement = &phy.Vec2f{
-			X: m.X(),
-			Y: m.Y(),
-		}
-	}
-
-	// parse Action if existent
-	a := fbInput.Action(nil)
-	if a != nil {
-		i.Action = &model.Action{
-			Item: items.ItemEnum(a.Item()),
-			Type: model.ActionType(a.ActionType()),
-		}
-	}
-	return i
-}
 
 func NewInputSystem(g *Game) *PlayerInputSystem {
 	return &PlayerInputSystem{game: g}
@@ -82,7 +55,10 @@ func (i *PlayerInputSystem) New(w *ecs.World) {
 		for {
 			select {
 			case msg := <-i.receive:
-				input := PlayerInputFlatbufferUnmarshal(msg.body)
+				input := codec.ClientMessageFlatbufferUnmarshal(msg.body)
+				if input == nil {
+					break
+				}
 				i.storeInput(msg.playerId, input)
 			}
 		}
