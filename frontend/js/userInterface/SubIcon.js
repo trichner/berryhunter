c0050be80ff10356e794230d559c48aed20d9924
R.Zander <r-zander@users.noreply.github.com>
2017-12-25 21:00:59 +0100

    Basic SubIcon implementation.

--- frontend/js/userInterface/SubIcon.js
+++ frontend/js/userInterface/SubIcon.js
@@ -1,9 +1,19 @@
-define([], function () {
+define(['Utils'], function (Utils) {
 	class SubIcon {
 		constructor(domElement, iconPath, count) {
-
+			domElement.getElementsByClassName('itemIcon').item(0).setAttribute('src', iconPath);
+			this.countElement = domElement.getElementsByClassName('count').item(0);
+			this.count = count;
 		}
 	}
+
+	Object.defineProperty(SubIcon.prototype, 'count', function (count) {
+		if (Utils.isDefined(count)) {
+			this.countElement.textContent = count;
+		} else {
+			return parseInt(this.countElement.textContent);
+		}
+	});
 
 	return SubIcon;
 });
