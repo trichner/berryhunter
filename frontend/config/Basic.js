c8e1cff65f01526bc40e84681051adcbbfaa95c8
Raoul Zander <hello@rza.io>
2018-03-18 14:50:51 +0100

    Synced movement speed with backend

--- frontend/config/Basic.js
+++ frontend/config/Basic.js
@@ -25,7 +25,7 @@ define([], function () {
 		 *
 		 * SYNCED WITH BACKEND
 		 */
-		BASE_MOVEMENT_SPEED: 10,
+        BASE_MOVEMENT_SPEED: 12,
 
 		/**
 		 * true: character movement AND mouse movement adjust the character facing direction
