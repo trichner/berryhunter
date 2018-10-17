85cfd177632834f6cb47692d2bc16cda71d66074
trojaxatbuk <38283627+trojaxatbuk@users.noreply.github.com>
2018-10-17 11:10:16 +0700

    gofmt used and everything updated still with main error
    placeable.go:14:30: cannot convert Placeable literal (type *Placeable) to type model.PlaceableEntity:
            *Placeable does not implement model.PlaceableEntity (wrong type for Update method)
                    have Update(float32) bool
                    want Update(float32)
    
    and
    
    if we remove:         definition *items.ItemDefinition
    
    two errors occur:
    model\placeable\placeable.go:114:3: unknown field 'definition' in struct literal of type Placeable
    model\placeable\placeable.go:130:20: p.definition undefined (type *Placeable has no field or method definition)

--- backend/model/placeable/placeable.go
+++ backend/model/placeable/placeable.go
@@ -1,170 +1,140 @@
 package placeable
 
 import (
-	"fmt"
-	"github.com/trichner/berryhunter/api/schema/BerryhunterApi"
-	"github.com/trichner/berryhunter/backend/items"
-	"github.com/trichner/berryhunter/backend/model"
-	"github.com/trichner/berryhunter/backend/model/vitals"
-	"github.com/trichner/berryhunter/backend/phy"
-	"log"
-	"math"
+        "fmt"
+        "github.com/trichner/berryhunter/api/schema/BerryhunterApi"
+        "github.com/trichner/berryhunter/backend/items"
+        "github.com/trichner/berryhunter/backend/model"
+        "github.com/trichner/berryhunter/backend/model/vitals"
+        "github.com/trichner/berryhunter/backend/phy"
+        "log"
+        "math"
 )
 
 var _ = model.PlaceableEntity(&Placeable{})
 
-//type PlaceableID uint64
-
 type Placeable struct {
-	model.BaseEntity
-	item       items.Item
-	health     vitals.VitalSign
-	definition *items.ItemDefinition
+        model.BaseEntity
+        item       items.Item
+        health     vitals.VitalSign
+        definition *items.ItemDefinition
 
-	radiator      *model.HeatRadiator
-	ticksLeft     int
-	statusEffects model.StatusEffects
+        radiator      *model.HeatRadiator
+        ticksLeft     int
+        statusEffects model.StatusEffects
 }
 
 func (p *Placeable) Update(dt float32) bool {
-	p.ticksLeft -= 1
-
-	// prevent underflow
-	if p.ticksLeft < 0 {
-		p.ticksLeft = 0
-	}
-
-	// will be required later to some capacity
-	//	for c := range auraCollisions {
-	//		p, ok := c.Shape().UserData.(model.PlayerEntity)
-	//		if ok {
-	//			if p.IsGod() {
-	//				continue
-	//			}
-	//			if p.definition.Factors.DamageFraction != 0 {
-	//				h := p.VitalSigns().Health.SubFraction(p.definition.Factors.DamageFraction)
-	//				p.VitalSigns().Health = h
-	//			}
-	//		}
-	//	}
-
-	return p.health > 0
+        p.ticksLeft -= 1
+        // prevent underflow
+        if p.ticksLeft < 0 {
+                p.ticksLeft = 0
+        }
+        return p.health > 0
 }
 
 func (p *Placeable) Decayed() bool {
-	return p.ticksLeft <= 0
+        return p.ticksLeft <= 0
 }
 
 func (p *Placeable) HeatRadiation() *model.HeatRadiator {
-	return p.radiator
+        return p.radiator
 }
 
 func (p *Placeable) SetPosition(pos phy.Vec2f) {
-	p.BaseEntity.SetPosition(pos)
-	if p.radiator != nil {
-		p.radiator.Body.SetPosition(pos)
-	}
+        p.BaseEntity.SetPosition(pos)
+        if p.radiator != nil {
+                p.radiator.Body.SetPosition(pos)
+        }
 }
 
 func (p *Placeable) Bodies() model.Bodies {
-	b := p.BaseEntity.Bodies()
-	if p.radiator != nil {
-		return append(b, p.radiator.Body)
-	}
-    return b
+        b := p.BaseEntity.Bodies()
+        if p.radiator != nil {
+                b = append(b, p.radiator.Body)
+        }
+        return b
 }
 
 func (p *Placeable) Item() items.Item {
-	return p.item
+        return p.item
 }
 
 func (p *Placeable) StatusEffects() *model.StatusEffects {
-	return &p.statusEffects
+        return &p.statusEffects
 }
 
 func NewPlaceable(item items.Item) (*Placeable, error) {
-    d := item.ItemDefinition
-    
-	if d == nil {
-		return nil, fmt.Errorf("No resource provided.")
-	}
+        d := item.ItemDefinition
 
-	if d.Body == nil {
-		return nil, fmt.Errorf("ItemDefinition is missing body property.")
-	}
+        if d == nil {
+                return nil, fmt.Errorf("No resource provided.")
+        }
 
-	body := phy.NewCircle(phy.VEC2F_ZERO, item.Body.Radius)
-	if item.Body.Solid {
-		body.Shape().Layer = int(model.LayerPlayerStaticCollision | model.LayerMobStaticCollision | model.LayerActionCollision | model.LayerViewportCollision)
-	} else {
-		body.Shape().Layer = int(model.LayerViewportCollision)
-		body.Shape().IsSensor = true
-	}
+        if d.Body == nil {
+                return nil, fmt.Errorf("ItemDefinition is missing body property.")
+        }
 
-	if body == nil {
-		return nil, fmt.Errorf("No body provided.")
-	}
+        body := phy.NewCircle(phy.VEC2F_ZERO, item.Body.Radius)
+        if item.Body.Solid {
+                body.Shape().Layer = int(model.LayerPlayerStaticCollision | model.LayerMobStaticCollision | model.LayerActionCollision | model.LayerViewportCollision)
+        } else {
+                body.Shape().Layer = int(model.LayerViewportCollision)
+                body.Shape().IsSensor = true
+        }
 
-	var radiator *model.HeatRadiator = nil
+        if body == nil {
+                return nil, fmt.Errorf("No body provided.")
+        }
 
-	if item.Factors.HeatPerTick != 0 {
-		radiator = &model.HeatRadiator{}
-		radiator.HeatPerTick = item.Factors.HeatPerTick
-		radiator.Radius = item.Factors.HeatRadius
-		heaterBody := phy.NewCircle(phy.VEC2F_ZERO, radiator.Radius)
-		heaterBody.Shape().IsSensor = true
-		heaterBody.Shape().Mask = int(model.LayerHeatCollision)
-		heaterBody.Shape().Group = -1 // no need to collide with other heat sources
-		radiator.Body = heaterBody
-	}
+        var radiator *model.HeatRadiator = nil
 
-	// setup the decay time
-	var ticksLeft int = math.MaxInt32
-	if item.Factors.DurationInTicks != 0 {
-		ticksLeft = item.Factors.DurationInTicks
-	}
+        if item.Factors.HeatPerTick != 0 {
+                radiator = &model.HeatRadiator{}
+                radiator.HeatPerTick = item.Factors.HeatPerTick
+                radiator.Radius = item.Factors.HeatRadius
+                heaterBody := phy.NewCircle(phy.VEC2F_ZERO, radiator.Radius)
+                heaterBody.Shape().IsSensor = true
+                heaterBody.Shape().Mask = int(model.LayerHeatCollision)
+                heaterBody.Shape().Group = -1 // no need to collide with other heat sources
+                radiator.Body = heaterBody
+        }
 
-	base := model.NewBaseEntity(body, BerryhunterApi.EntityTypePlaceable)
-	p := &Placeable{
-		BaseEntity:    base,
-		item:          item,
-        health:        vitals.Max,
-		definition:    d,
-		radiator:      radiator,
-		ticksLeft:     ticksLeft,
-		statusEffects: model.NewStatusEffects(),
-	}
-	p.Body.Shape().UserData = p
-	return p, nil
+        // setup the decay time
+        var ticksLeft int = math.MaxInt32
+        if item.Factors.DurationInTicks != 0 {
+                ticksLeft = item.Factors.DurationInTicks
+        }
+
+        base := model.NewBaseEntity(body, BerryhunterApi.EntityTypePlaceable)
+        p := &Placeable{
+                BaseEntity:    base,
+                item:          item,
+                health:        vitals.Max,
+                definition:    d,
+                radiator:      radiator,
+                ticksLeft:     ticksLeft,
+                statusEffects: model.NewStatusEffects(),
+        }
+        p.Body.Shape().UserData = p
+        return p, nil
 }
 
-// did you mean this wasnt necessary?
-//func (p *Placeable) PlaceableID() placeables.PlaceableID {
-//	return p.definition.ID
-//}
-
 func (p *Placeable) Health() vitals.VitalSign {
-	return p.health
+        return p.health
 }
 
 func (p *Placeable) PlayerHitsWith(player model.PlayerEntity, item items.Item) {
-	log.Printf("🎯")
-    
-	vulnerability := p.definition.Factors.Vulnerability
-	if vulnerability == 0 {
-		vulnerability = 1
-	}
+        log.Printf("🎯")
 
-	dmgFraction := item.Factors.Damage * vulnerability
-	if dmgFraction > 0 {
-		p.health = p.health.SubFraction(dmgFraction)
-		p.StatusEffects().Add(BerryhunterApi.StatusEffectDamaged)
+        vulnerability := p.definition.Factors.Vulnerability
+        if vulnerability == 0 {
+                vulnerability = 1
+        }
 
-		// can be reused, when drops are set for placeables
-//		if p.health <= 0 {
-//			for _, i := range p.definition.Drops {
-//				player.Inventory().AddItem(i)
-//			}
-//		}
-	}
+        dmgFraction := item.Factors.Damage * vulnerability
+        if dmgFraction > 0 {
+                p.health = p.health.SubFraction(dmgFraction)
+                p.StatusEffects().Add(BerryhunterApi.StatusEffectDamaged)
+        }
 }
