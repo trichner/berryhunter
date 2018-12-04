9be65708aa2544dd95b2ae9352b76e4ba59f44f2
Raoul Zander <hello@rza.io>
2018-12-04 10:29:47 +0100

    Sorted changelogs by date.

--- frontend/src/ts/userInterface/screens/Changelog.ts
+++ frontend/src/ts/userInterface/screens/Changelog.ts
@@ -10,10 +10,39 @@ const changelogs = requireAll(require.context('../../../../../changelog', false,
 
 const template = require('raw-loader!./changelog.mustache');
 
-function mapChangelogs(changelogs) {
-    // TODO sort changelogs by date descending
-    return changelogs.map((changelog) => {
-        let categories = {};
+interface ChangelogJSON {
+    date: string,
+    codename: string,
+    describtion?: string,
+    changes: ChangeJSON[]
+}
+
+interface ChangeJSON {
+    category: string,
+    describtion: string,
+    trelloId: string | string[]
+}
+
+interface ChangelogVO {
+    codename: string,
+    date: moment.Moment,
+    datetime: string,
+    dateFormatted: string,
+    categories: ChangeCategoryVO[]
+}
+
+interface ChangeCategoryVO {
+    name: string,
+    changes: ChangeVO[]
+}
+
+interface ChangeVO {
+
+}
+
+function mapChangelogs(changelogs: ChangelogJSON[]) : ChangelogVO[] {
+    let mapped: ChangelogVO[] = changelogs.map((changelog) => {
+        let categories = newCategories;
         changelog.changes.forEach((change) => {
             let category = categories[change.category];
 
@@ -30,11 +59,32 @@ function mapChangelogs(changelogs) {
         let mdate = moment(changelog.date, 'DD.MM.YYYY');
         return {
             codename: changelog.codename,
+            date: mdate,
             datetime: mdate.format('YYYY-MM-DD'),
             dateFormatted: mdate.format('L'),
-            categories: Object.values(categories)
+            // TODO spread map with .values()
+            categories: Object.values( categories)
         };
-    })
+    });
+
+    mapped.sort((c1, c2) => {
+        // Sort descending - newest on top
+        return -1 * c1.date.diff(c2.date);
+    });
+
+    return mapped;
+}
+
+function newCategories() : Map<string, ChangeCategoryVO[]> {
+    let categories = new Map<string, ChangeCategoryVO[]>();
+
+    // Insertion order defines sorting
+    categories.set('Highlight', []);
+    categories.set('Feature', []);
+    categories.set('Bug Fix', []);
+    categories.set('Improvement', []);
+
+    return categories;
 }
 
 function renderChangelogs(changelogs) {
