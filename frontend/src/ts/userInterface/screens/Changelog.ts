8485709c722bd838401cc8046f1f9077b057dba2
Raoul Zander <hello@rza.io>
2018-12-07 01:39:32 +0100

    Changelog can now be displayed on the start screen.

--- frontend/src/ts/userInterface/screens/Changelog.ts
+++ frontend/src/ts/userInterface/screens/Changelog.ts
@@ -1,14 +1,77 @@
 import * as Mustache from 'mustache';
 import * as moment from 'moment';
-import {isUndefined} from "../../Utils";
+import {isUndefined} from '../../Utils';
+import * as Events from '../../Events';
+import {createStartScreenPanel} from "./ScreenUtil";
 
 function requireAll(requireContext) {
     return requireContext.keys().map(requireContext);
 }
 
 const changelogs = requireAll(require.context('../../../../../changelog', false, /\.json$/));
-
 const template = require('raw-loader!./changelog.mustache');
+
+Events.on('startScreen.domReady', setup);
+
+function setup() {
+    createStartScreenPanel(
+        document.getElementById('changelog'),
+        renderChangelogs(mapChangelogs(changelogs)),
+        'closeChangelog',
+        'changelogVisible',
+        '#changelog');
+}
+
+function renderChangelogs(changelogs: ChangelogVO[]): string {
+    return Mustache.render(template, changelogs);
+}
+
+function mapChangelogs(changelogs: ChangelogJSON[]): ChangelogVO[] {
+    let mapped: ChangelogVO[] = changelogs.map((changelog) => {
+        let categories = newCategories();
+        changelog.changes.forEach((change) => {
+            let category = categories.get(change.category);
+            if (isUndefined(category)) {
+                throw 'Unknown change category "' + change.category + '"';
+            }
+            category.changes.push(change);
+        });
+        let mdate = moment(changelog.date, 'DD.MM.YYYY');
+        return {
+            codename: changelog.codename,
+            date: mdate,
+            datetime: mdate.format('YYYY-MM-DD'),
+            dateFormatted: mdate.format('L'),
+            categories: Array.from(categories.values())
+        };
+    });
+
+    mapped.sort((c1, c2) => {
+        // Sort descending - newest on top
+        return -1 * c1.date.diff(c2.date);
+    });
+
+    return mapped;
+}
+
+function newCategories(): Map<string, ChangeCategoryVO> {
+    let categories = new Map<string, ChangeCategoryVO>();
+
+    // Insertion order defines sorting
+    newCategory(categories, 'highlight', 'Highlights in this version');
+    newCategory(categories, 'feature', 'New Features');
+    newCategory(categories, 'bugFix', 'Fixed Bugs');
+    newCategory(categories, 'improvement', 'Other improvements and changes');
+
+    return categories;
+}
+
+function newCategory(categories: Map<string, ChangeCategoryVO>, key: string, name: string) {
+    categories.set(key, {
+        name: name,
+        changes: []
+    });
+}
 
 interface ChangelogJSON {
     date: string,
@@ -37,60 +100,7 @@ interface ChangeCategoryVO {
 }
 
 interface ChangeVO {
-
+    category: string,
+    describtion: string,
+    trelloId: string | string[]
 }
-
-function mapChangelogs(changelogs: ChangelogJSON[]) : ChangelogVO[] {
-    let mapped: ChangelogVO[] = changelogs.map((changelog) => {
-        let categories = newCategories;
-        changelog.changes.forEach((change) => {
-            let category = categories[change.category];
-
-            if (isUndefined(category)) {
-                category = {
-                    name: change.category,
-                    changes: []
-                };
-                categories[change.category] = category;
-            }
-
-            category.changes.push(change);
-        });
-        let mdate = moment(changelog.date, 'DD.MM.YYYY');
-        return {
-            codename: changelog.codename,
-            date: mdate,
-            datetime: mdate.format('YYYY-MM-DD'),
-            dateFormatted: mdate.format('L'),
-            // TODO spread map with .values()
-            categories: Object.values( categories)
-        };
-    });
-
-    mapped.sort((c1, c2) => {
-        // Sort descending - newest on top
-        return -1 * c1.date.diff(c2.date);
-    });
-
-    return mapped;
-}
-
-function newCategories() : Map<string, ChangeCategoryVO[]> {
-    let categories = new Map<string, ChangeCategoryVO[]>();
-
-    // Insertion order defines sorting
-    categories.set('Highlight', []);
-    categories.set('Feature', []);
-    categories.set('Bug Fix', []);
-    categories.set('Improvement', []);
-
-    return categories;
-}
-
-function renderChangelogs(changelogs) {
-    console.log(changelogs);
-    let rendered = Mustache.render(template, changelogs);
-    console.log(rendered);
-}
-
-renderChangelogs(mapChangelogs(changelogs));
