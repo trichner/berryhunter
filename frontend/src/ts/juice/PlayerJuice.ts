0919d4a942c0afa829f420d136b67f4515f30205
Nullformed <92059356+Nullformed@users.noreply.github.com>
2024-06-23 18:17:48 +0200

    Added eating sound on ConsumeItem

--- frontend/src/ts/juice/PlayerJuice.ts
+++ frontend/src/ts/juice/PlayerJuice.ts
@@ -1,8 +1,9 @@
-import {InventoryAddEvent, BeforeDeathEvent, PlaceablePlacedEvent} from '../Events';
+import {InventoryAddEvent, BeforeDeathEvent, PlaceablePlacedEvent, ControlsActionEvent} from '../Events';
 import {random} from '../Utils';
 import {sound} from '@pixi/sound';
 import * as PIXI from 'pixi.js';
 import {registerPreload} from '../Preloading';
+import {BerryhunterApi} from "../backend/BerryhunterApi";
 
 BeforeDeathEvent.subscribe((payload) => {
         sound.play('death', {
@@ -34,11 +35,24 @@ PlaceablePlacedEvent.subscribe((payload) => {
     }
 });
 
+ControlsActionEvent.subscribe((payload) => {
+    switch (payload.actionType) {
+        case BerryhunterApi.ActionType.ConsumeItem:
+            sound.play('eat', {
+                speed: random(0.9, 1.11),
+                volume: random(0.8, 1),
+            });
+            break;
+    }
+});
+
 PIXI.Assets.add({alias: 'collect', src: require('../../sounds/245645__unfa__cartoon-pop-clean.mp3')});
 PIXI.Assets.add({alias: 'death', src: require('../../sounds/416838__tonsil5__grunt2-death-pain.mp3')});
 PIXI.Assets.add({alias: 'place-heavy', src: require('../../sounds/443629__checholio__28-clavando-estaca.mp3')});
+PIXI.Assets.add({alias: 'eat', src: require('../../sounds/548367__borgory__chewing-crunch.mp3')});
 
 // noinspection JSIgnoredPromiseFromCall
 registerPreload(PIXI.Assets.load('collect'));
 registerPreload(PIXI.Assets.load('death'));
 registerPreload(PIXI.Assets.load('place-heavy'));
+registerPreload(PIXI.Assets.load('eat'));
