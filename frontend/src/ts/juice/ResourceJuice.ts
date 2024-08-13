8c7b83f58061ebcf73477b3c0a3db4eff70bd17f
Nullformed <92059356+Nullformed@users.noreply.github.com>
2024-08-13 09:24:18 +0200

    [New] Spatial audio for resources

--- frontend/src/ts/juice/ResourceJuice.ts
+++ frontend/src/ts/juice/ResourceJuice.ts
@@ -1,9 +1,9 @@
-import {ResourceStockChangedEvent} from '../Events';
+import { ResourceStockChangedEvent } from '../Events';
 import * as Resources from '../gameObjects/Resources';
-import {isUndefined, random} from '../Utils';
-import {sound} from '@pixi/sound';
+import { isUndefined, random } from '../Utils';
+import { sound } from '@pixi/sound';
 import * as PIXI from 'pixi.js';
-import {registerPreload} from '../Preloading';
+import { registerPreload } from '../Preloading';
 import { spatialAudio } from './SpatialAudio';
 
 ResourceStockChangedEvent.subscribe((payload) => {
@@ -20,24 +20,27 @@ ResourceStockChangedEvent.subscribe((payload) => {
     switch (payload.entityType) {
         case Resources.MarioTree.name:
         case Resources.RoundTree.name:
-            spatialAudio.play('tree-chop', payload.position, {
+            spatialAudio.play('tree-chop',
+                payload.position, {
                 speed: random(0.9, 1.11),
                 volume: random(0.8, 1.25),
             });
             break;
         case Resources.BerryBush.name:
         case Resources.Flower.name:
-                sound.play('bush-hit', {
-                    speed: random(0.7, 0.9),
-                    volume: random(0.2, 0.3),
-                });
-                break;
+            spatialAudio.play('bush-hit',
+                payload.position, {
+                speed: random(0.7, 0.9),
+                volume: random(0.2, 0.3),
+            });
+            break;
         case Resources.Mineral.name:
         case Resources.Stone.name:
         case Resources.Bronze.name:
         case Resources.Iron.name:
         case Resources.Titanium.name:
-            sound.play('mineral-hit-dull', {
+            spatialAudio.play('mineral-hit-dull',
+                payload.position, {
                 speed: random(0.7, 1.3),
                 volume: random(1, 1.25),
             });
@@ -48,10 +51,10 @@ ResourceStockChangedEvent.subscribe((payload) => {
 });
 
 
-PIXI.Assets.add({alias: 'tree-chop', src: require('../../sounds/536736__egomassive__chop.mp3')});
-PIXI.Assets.add({alias: 'mineral-hit-dull', src: require('../../sounds/319229__worthahep88__single-rock-hit-dirt-2.mp3')});
-PIXI.Assets.add({alias: 'mineral-hit-sharp', src: require('../../sounds/390770__d00121058__fx_014_sword_contact_stone_2.mp3')});
-PIXI.Assets.add({alias: 'bush-hit', src: require('../../sounds/637791__kyles__rock-hit-bushes-brush-land-in-leaves.mp3')});
+PIXI.Assets.add({ alias: 'tree-chop', src: require('../../sounds/536736__egomassive__chop.mp3') });
+PIXI.Assets.add({ alias: 'mineral-hit-dull', src: require('../../sounds/319229__worthahep88__single-rock-hit-dirt-2.mp3') });
+PIXI.Assets.add({ alias: 'mineral-hit-sharp', src: require('../../sounds/390770__d00121058__fx_014_sword_contact_stone_2.mp3') });
+PIXI.Assets.add({ alias: 'bush-hit', src: require('../../sounds/637791__kyles__rock-hit-bushes-brush-land-in-leaves.mp3') });
 // noinspection JSIgnoredPromiseFromCall
 registerPreload(PIXI.Assets.load('tree-chop'));
 registerPreload(PIXI.Assets.load('mineral-hit-dull'));
