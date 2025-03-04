0a197b37cf6ee12331932d174a6510630dfdb127
R. Zander <r-zander@users.noreply.github.com>
2025-03-05 00:53:28 +0100

    Fixed 268-right-clicking-the-inventory-shows-the-context-menu-but-should-not

--- frontend/src/features/user-interface/HUD/logic/ClickableCountableIcon.ts
+++ frontend/src/features/user-interface/HUD/logic/ClickableCountableIcon.ts
@@ -1,23 +1,20 @@
 import {ClickableIcon} from './ClickableIcon';
 
 export class ClickableCountableIcon extends ClickableIcon {
-    countNode;
+    private countNode: HTMLElement;
 
-    /**
-     * @param {Element} node
-     */
-    constructor(node) {
+    constructor(node: Element) {
         super(node);
 
         this.countNode = this.domElement.querySelector('.count');
     }
 
-    setCount(count) {
+    setCount(count: number) {
         if (count <= 1) {
             this.countNode.classList.add('hidden');
         } else {
             this.countNode.classList.remove('hidden');
-            this.countNode.textContent = count;
+            this.countNode.textContent = String(count);
         }
     }
 }
